8fa21810da517b6be7d8618632d4618e20a18216
Kade Thompson <kadebthompson3@gmail.com>
2020-03-12 09:53:38 -0600

    afternoon-project-js3

--- nesting.js
+++ nesting.js
@@ -51,7 +51,16 @@ var employees = [
 */
 
 //Code Here
-
+function employeeUpdater(){
+  for(i = 0; i < employees.length; i++){
+    if(employees[i].firstName === 'Theo'){
+      delete employees[i]
+    } else if(employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+  }
+  return employees
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -68,6 +77,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+function removeDuplicates(workplaceAccidents){
+  for(let i = 0; i < workplaceAccidents.length; i++){
+    for(let j = workplaceAccidents.length - 1; j > i; j--){
+      if (workplaceAccidents[j] === workplaceAccidents[i]){
+        workplaceAccidents.splice(j, 1)
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 
@@ -96,8 +115,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]
+var fluffy2ndFriend = cat.catFriends[1].name
 
 
 
@@ -139,6 +158,24 @@ var myCar = {
 
 //Code Here
 
+function recordCleaner(){
+  for (var i = 0; i < myCar.accidents.length; i++) {
+    if(myCar.accidents[i].atFaultForAccident = true){
+    myCar.accidents[i].atFaultForAccident = false
+  }
+  }
+  console.log(myCar)
+}
+// function recordCleaner(){
+//   for(let key in myCar){
+//     if(myCar[key] === true){
+//       myCar[key] = false
+//     }
+//   }
+// }
+  
+
+
 
 
 ////////// PROBLEM 5 //////////
@@ -157,18 +194,17 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
-function looper(numsArr){
-  for(let i = 0; i < numsArr.length; i++){
+const looper = () => {
+  for (let i = 0; i < numsArr.length; i++){
     for(let j = 0; j < numsArr[i].length; j++){
-      if(numsArr[i][j] % 2 === 0){
-        numsArr[i][j] = 'even'
-      } else if(numsArr[i][j] % 2 === !0){
-       numsArr[i][j] = 'odd'
-      }
+    if (numsArr[i][j] % 2 === 0){
+      numsArr[i][j] = 'even'
+    } else if (numsArr[i][j] % 2 === 1){
+      numsArr[i][j] = 'odd'
     }
   }
-  
-  return numsArr
 }
-console.log(numsArr)
-
+  return numsArr;
+}
+looper();
+   
